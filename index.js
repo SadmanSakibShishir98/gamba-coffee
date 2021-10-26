0f651b82b2ab31bfb597947745f62244ca98ce1e
SadmanSakibShishir98 <sadmansakibshishir98@gmail.com>
2021-10-26 20:38:26 +0600

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const coldCoffee = document.getElementById("cold-coffee"),
+//         hotCoffee= document.getElementById("hot-coffee"),
+//         btnHot=document.getElementById("hot-coffee-btn"),
+//         btnCold=document.getElementById("cold-coffee-btn");
+
+//         coldCoffee.addEventListener("click",coffeeCold);
+//         hotCoffee.addEventListener("click",coffeeHot);
+
+//         function coffeeCold(){
+
+//         }
